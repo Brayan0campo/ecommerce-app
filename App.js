546286659afd89f7f157b1a181aae8c1c58874cf
Brayan <ocamporodriguezbrayan@gmail.com>
2024-06-22 00:07:10 -0500

    chore: update app file and image welcome

---
+++ App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { MainNavigator } from "./navigation/mainNavigator";
+import { init } from "./db";
+import Toast from "react-native-toast-message";
+
+SplashScreen.preventAutoHideAsync();
+
+init()
+  .then(() => console.log("Database initialized"))
+  .catch((err) => console.error("Database initialization failed", err));
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Unbounded-Bold": require("./assets/fonts/Unbounded-Bold.ttf"),
+    "Unbounded-Regular": require("./assets/fonts/Unbounded-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    const onLayoutRootView = async () => {
+      if (fontsLoaded || fontError) {
+        await SplashScreen.hideAsync();
+      }
+    };
+    onLayoutRootView();
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) return null;
+  return (
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <StatusBar backgroundColor="white" style="dark" />
+        <MainNavigator />
+        <Toast />
+      </SafeAreaProvider>
+    </Provider>
+  );
+}
---
+++ firebase/database.js
@@ -0,0 +1,3 @@
+export const apiKey = "AIzaSyB-_Qk4fGWjsw4-Qt0cF0PslnVucpx38tM";
+export const authUrl = "https://identitytoolkit.googleapis.com/";
+export const baseUrl = "https://ecommerce-27dd4-default-rtdb.firebaseio.com/";
