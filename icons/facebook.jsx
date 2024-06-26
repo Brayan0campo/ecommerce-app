df23ff4cfdd3348c974faa3fc1c76d2abdd01509
Brayan <ocamporodriguezbrayan@gmail.com>
2024-06-25 19:57:26 -0500

    chore: add icons

---
+++ icons/facebook.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+
+export const FacebookIcon = (props) => (
+  <Svg width={24} height={24} fill="none" {...props}>
+    <Path
+      fill="#1877F2"
+      d="M22.5 12c0-5.8-4.7-10.5-10.5-10.5S1.5 6.2 1.5 12c0 5.3 3.9 9.7 8.9 10.4v-7.3H7.5v-3h2.9v-2.3c0-2.9 1.8-4.4 4.3-4.4 1.2 0 2.4.1 2.4.1v2.8H15c-1.2 0-1.5.6-1.5 1.5v1.9h3l-.4 3h-2.6v7.3c5-.7 8.9-5.1 8.9-10.4z"
+    />
+  </Svg>
+);
